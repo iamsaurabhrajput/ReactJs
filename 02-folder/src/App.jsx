e9712d6d5904ee7f1a01d898c116bda8771db2cf
iamsaurabhrajput <sr7944692@gmail.com>
2026-07-29 14:28:10 +0530

    this is second react folder

---
+++ 02-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card';
+
+const App = () => {
+  const age = 20;
+  return (
+    <div>
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () =>{
+  return (
+    <div className='full'>
+      <div className='header'>
+        <img src="https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_640.jpg" alt="" />
+      </div>
+      <div className='main'>
+        <h1>SAURABH RAJPUT</h1>
+        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+      </div>
+      <div className='footer'>
+        <button className='butt'>
+          View Profile
+        </button>
+      </div>
+    </div> 
+  )
+}
+
+export default Card
